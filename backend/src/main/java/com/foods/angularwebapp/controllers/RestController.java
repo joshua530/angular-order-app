8a06e6fb8975b4c429522690a764416accf3a7a3
joshua530 <joshuaomari5@gmail.com>
2022-03-19 16:57:07 +0300

    implement request forwarding
    request will be forwarded to / instead of being redirected

--- backend/src/main/java/com/foods/angularwebapp/controllers/RestController.java
+++ backend/src/main/java/com/foods/angularwebapp/controllers/RestController.java
@@ -7,22 +7,16 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RestController implements ErrorController {
   @RequestMapping("/error")
-  public void handleError(HttpServletRequest request, HttpServletResponse response) {
-    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-    if (status != null) {
-      Integer statusCode = Integer.valueOf(status.toString());
-      if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-        response.addHeader("Location", "/500.html");
-      } else {
-        response.addHeader("Location", "/");
-      }
-      response.setStatus(302);
-    }
+  public ModelAndView redirectWithUsingForwardPrefix(ModelMap model) {
+    model.addAttribute("attribute", "forwardWithForwardPrefix");
+    return new ModelAndView("forward:/", model);
   }
 }

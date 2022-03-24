4d45000b0be842891af635794ac7e68f4dba41fb
joshua530 <joshuaomari5@gmail.com>
2022-03-24 22:21:16 +0300

    delete non used imports

--- backend/src/main/java/com/foods/angularwebapp/controllers/RestController.java
+++ backend/src/main/java/com/foods/angularwebapp/controllers/RestController.java
@@ -1,14 +1,8 @@
 package com.foods.angularwebapp.controllers;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 

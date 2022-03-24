68ddf260acb8034ff7a6746599b573397e9ba103
joshua530 <joshuaomari5@gmail.com>
2022-03-24 22:18:44 +0300

    update db config

--- backend/src/main/java/com/foods/angularwebapp/config/DbConfig.java
+++ backend/src/main/java/com/foods/angularwebapp/config/DbConfig.java
@@ -1,149 +1,201 @@
-// package com.foods.angularwebapp.config;
+package com.foods.angularwebapp.config;
 
-// import com.foods.angularwebapp.repositories.OrderRepository;
-// import com.foods.angularwebapp.repositories.ProductRepository;
-// import com.foods.angularwebapp.repositories.UserRepository;
-// import com.foods.angularwebapp.security.Encryption;
+import com.foods.angularwebapp.repositories.OrderRepository;
+import com.foods.angularwebapp.repositories.ProductRepository;
+import com.foods.angularwebapp.repositories.UserRepository;
+import com.foods.angularwebapp.security.Encryption;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.boot.CommandLineRunner;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// import java.util.UUID;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
-// import com.foods.angularwebapp.models.*;
-// import com.foods.angularwebapp.utils.OrderStatus;
+import com.foods.angularwebapp.models.*;
+import com.foods.angularwebapp.utils.OrderStatus;
 
-// @Configuration
-// public class DbConfig {
-// @Value("${application.secretkey}")
-// private String salt;
+@Configuration
+public class DbConfig {
+        @Value("${application.secretkey}")
+        private String salt;
 
-// @Autowired
-// private Encryption encryption;
+        @Autowired
+        private Encryption encryption;
 
-// @Bean
-// CommandLineRunner cmd(ProductRepository pr, UserRepository ur,
-// OrderRepository or) {
+        /**
+         * generates a random double for a product's price
+         */
+        private Double generatePrice() {
+                double random = ThreadLocalRandom.current().nextDouble(12.99, 40.58);
+                int tmp = (int) (random * 100);
+                double price = ((double) tmp) / 100;
+                return price;
+        }
 
-// return args -> {
-// Product p = new Product();
-// p.setId(1);
-// Product p2 = new Product();
-// p2.setId(2);
-// User u = new User();
-// u.setId(1);
-// User u2 = new User();
-// u2.setId(2);
+        /**
+         * generates a random time string
+         */
+        private String generateTime() {
+                SimpleDateFormat formatter = new SimpleDateFormat("dd MMM YYYY h:mm a");
+                Date date = new Date();
+                // generate a random date between January 20, 2022 and now
+                String time = formatter.format(
+                                new Date(
+                                                (int) ((Math.random() * (date.getTime() - 1642661718)) + 1642661718)));
+                return time;
+        }
 
-// User john = new User("john", true);
-// john.setAdmin(true);
-// john.setStrId(UUID.randomUUID().toString());
-// String encryptedPass = encryption.encryptPassword("password", salt);
-// john.setPassword(encryptedPass);
-// john.setUsername("john");
+        @Bean
+        CommandLineRunner cmd(ProductRepository pr, UserRepository ur,
+                        OrderRepository or) {
 
-// User alice = new User();
-// alice.setUsername("alice");
-// alice.setStrId(UUID.randomUUID().toString());
+                return args -> {
+                        Product p = new Product();
+                        p.setId(1);
+                        Product p2 = new Product();
+                        p2.setId(2);
+                        User u = new User();
+                        u.setId(1);
+                        User u2 = new User();
+                        u2.setId(2);
 
-// User jane = new User();
-// jane.setUsername("jane");
-// alice.setStrId(UUID.randomUUID().toString());
+                        User john = new User("john", true);
+                        john.setAdmin(true);
+                        john.setStrId(UUID.randomUUID().toString());
+                        String encryptedPass = encryption.encryptPassword("password", salt);
+                        john.setPassword(encryptedPass);
+                        john.setUsername("john");
 
-// ur.save(john);
-// ur.save(alice);
-// ur.save(jane);
+                        User alice = new User();
+                        alice.setUsername("alice");
+                        alice.setStrId(UUID.randomUUID().toString());
 
-// Product food = new Product(
-// "Food",
-// 12.23, "Delicious food",
-// "/images/food.jpg",
-// "Contains vitamins A,C and D");
+                        User jane = new User();
+                        jane.setUsername("jane");
+                        alice.setStrId(UUID.randomUUID().toString());
 
-// Product pancakes = new Product(
-// "Pancakes",
-// 20.11, "Pancakes covered with honey",
-// "/images/food6.jpg",
+                        ur.save(john);
+                        ur.save(alice);
+                        ur.save(jane);
 
-// "Rich in vitamins. Perfect way to start your day"
+                        String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id nulla sit amet nulla dictum placerat. Vivamus lacinia ornare vehicula. Ut consectetur massa justo. Sed non fermentum eros, sit amet pulvinar lacus. Donec facilisis aliquam condimentum. Ut et venenatis sapien, vel ornare neque. Suspendisse sit amet egestas erat, quis tempus nisl.";
+                        String nutritionInfo = "In eget porta nulla. Sed dignissim neque leo, nec accumsan quam accumsan vel. Sed condimentum congue eros. Suspendisse sagittis risus ligula, nec semper ante imperdiet suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras blandit, massa id vulputate fermentum, erat tellus rhoncus justo, et facilisis tellus eros id nisl. Suspendisse fringilla sem ut tortor sagittis viverra. Etiam convallis vitae felis nec congue.";
 
-// );
-// Product mixedSalad = new Product(
-// "Mixed salad", 32.16,
-// "Salad composed of different fruits and vegetables",
-// "/images/food5.jpg",
+                        String staticUrl = "http://localhost/static";
 
-// "All manner of vitamins, ranging from A to E are to be found here"
+                        // featured
+                        Product pizza = new Product(
+                                        "pizza",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/pizza.png", nutritionInfo, true, false);
+                        Product chicken = new Product(
+                                        "chicken",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/chicken.png",
+                                        nutritionInfo, true, false);
+                        Product yogurt = new Product(
+                                        "yogurt",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/yogurt.png",
+                                        nutritionInfo, true, false);
+                        Product milkShake = new Product(
+                                        "milk shake",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/milk_shake.png",
+                                        nutritionInfo, true, false);
 
-// );
-// Product sandwich = new Product(
-// "Bacon sandwich", 18.45, "Delicious bacon sandwich",
-// "/images/food4.jpg",
+                        // hot
+                        Product roastedRibs = new Product(
+                                        "roasted ribs",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/roasted_ribs.png", nutritionInfo, false, true);
+                        Product hotDog = new Product(
+                                        "hot dog",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/hot_dog.png", nutritionInfo, false, true);
+                        Product fish = new Product(
+                                        "fish",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/fish.png", nutritionInfo, false, true);
+                        Product friedRice = new Product(
+                                        "fried rice",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/fried_rice.png", nutritionInfo, false, true);
 
-// "Crunchy with a touch of healthy vegetables. A good snack to grab when" +
-// " in need of a meal"
+                        // others
+                        Product rice = new Product(
+                                        "rice",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/rice.png", nutritionInfo, false, false);
+                        Product coffee = new Product(
+                                        "coffee",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/coffee_latte.png", nutritionInfo, false, false);
+                        Product sandwich = new Product(
+                                        "sandwich",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/sandwich.png", nutritionInfo, false, false);
+                        Product fruitSalad = new Product(
+                                        "fruit salad",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/fruit_salad.png", nutritionInfo, false, false);
+                        Product omelette = new Product(
+                                        "omelette",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/omelette.png", nutritionInfo, false, false);
+                        Product cookies = new Product(
+                                        "cookies",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/cookies.png", nutritionInfo, false, false);
+                        Product burger = new Product(
+                                        "burger",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/burger.png", nutritionInfo, false, false);
+                        Product pasta = new Product(
+                                        "pasta",
+                                        generatePrice(), description,
+                                        staticUrl + "/images/pasta.png", nutritionInfo, false, false);
 
-// );
-// Product sausages = new Product(
-// "Sausages", 22.89, "Golden brown barbecued sausages",
-// "/images/food7.jpg",
+                        pr.save(pizza);
+                        pr.save(chicken);
+                        pr.save(yogurt);
+                        pr.save(milkShake);
+                        pr.save(roastedRibs);
+                        pr.save(hotDog);
+                        pr.save(fish);
+                        pr.save(friedRice);
+                        pr.save(rice);
+                        pr.save(coffee);
+                        pr.save(sandwich);
+                        pr.save(fruitSalad);
+                        pr.save(omelette);
+                        pr.save(cookies);
+                        pr.save(burger);
+                        pr.save(pasta);
 
-// "A rich source of proteins. Ideal for people working towards gaining body" +
-// "mass"
+                        Order order1 = new Order(generateTime(), 3, OrderStatus.DELIVERED, jane, rice);
+                        Order order2 = new Order(generateTime(), 23, OrderStatus.PENDING, alice, roastedRibs);
+                        Order order3 = new Order(generateTime(), 3, OrderStatus.PENDING, jane, sandwich);
+                        Order order4 = new Order(generateTime(), 23, OrderStatus.PENDING, alice,
+                                        cookies);
+                        Order order5 = new Order(generateTime(), 3, OrderStatus.PROCESSING, jane, milkShake);
+                        Order order6 = new Order(generateTime(), 3, OrderStatus.DELIVERED, jane, sandwich);
+                        Order order7 = new Order(generateTime(), 23, OrderStatus.PENDING, alice, coffee);
+                        Order order8 = new Order(generateTime(), 23, OrderStatus.PENDING, alice, chicken);
 
-// );
-// Product rice = new Product(
-// "Beef steak with rice and broccoli", 44.38, "Scrumptious meal. The picture" +
-// "tells it all",
-// "/images/food2.jpg",
-
-// "Contains all the three major nutrients. A heavy meal that will come in" +
-// "handy" +
-// " anytime you feel hungry"
-
-// );
-// Product eggs = new Product(
-// "Eggs and salad", 23.33,
-// "Boiled eggs accompanied with a fruit and vegetable mixture, sprinkled with"
-// +
-// "black pepper",
-// "/images/food3.jpg",
-
-// "Contains proteins, vitamins and essential minerals. Don\"t forget the black"
-// +
-// "pepper."
-
-// );
-
-// pr.save(rice);
-// pr.save(food);
-// pr.save(pancakes);
-// pr.save(eggs);
-// pr.save(sausages);
-// pr.save(sandwich);
-// pr.save(mixedSalad);
-
-// Order order1 = new Order("Time", 3, OrderStatus.DELIVERED, jane, rice);
-// Order order2 = new Order("Time 2", 23, OrderStatus.PENDING, alice, pancakes);
-// Order order3 = new Order("Time", 3, OrderStatus.PENDING, jane, sandwich);
-// Order order4 = new Order("Time 2", 23, OrderStatus.PENDING, alice,
-// mixedSalad);
-// Order order5 = new Order("Time", 3, OrderStatus.PROCESSING, jane, eggs);
-// Order order6 = new Order("Time", 3, OrderStatus.DELIVERED, jane, sandwich);
-// Order order7 = new Order("Time 2", 23, OrderStatus.PENDING, alice, food);
-// Order order8 = new Order("Time 2", 23, OrderStatus.PENDING, alice, eggs);
-
-// or.save(order1);
-// or.save(order2);
-// or.save(order3);
-// or.save(order4);
-// or.save(order5);
-// or.save(order6);
-// or.save(order7);
-// or.save(order8);
-// };
-// }
-// }
+                        or.save(order1);
+                        or.save(order2);
+                        or.save(order3);
+                        or.save(order4);
+                        or.save(order5);
+                        or.save(order6);
+                        or.save(order7);
+                        or.save(order8);
+                };
+        }
+}
